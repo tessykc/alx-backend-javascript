bd344dcadc2ba8ed6ea1559b78df2d8bc6ec3b11
tessykc <tessykc@gmail.com>
2024-05-01 11:40:09 +0100

    Type

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,18 +1,18 @@
 function createInt8TypedArray(length, position, value) {
-    if (position < 0 || position >= length) {
-        throw new Error("Position outside range");
-    }
+  if (position < 0 || position >= length) {
+    throw new Error("Position outside range");
+  }
 
-    const buffer = new ArrayBuffer(length);
-    const int8Array = new Int8Array(buffer);
-    int8Array[position] = value;
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+  int8Array[position] = value;
 
-    return buffer;
+  return buffer;
 }
 
 // Example usage:
 try {
-    const resultBuffer = createInt8TypedArray(10, 2, 89);
+  const resultBuffer = createInt8TypedArray(10, 2, 89);
     console.log(resultBuffer);
 } catch (error) {
     console.error(error.message);

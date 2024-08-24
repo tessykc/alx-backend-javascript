c927f2c82dcd6b71a00ea824b275c73bf87cda6d
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2024-08-24 11:39:34 +0100

    Create 6-http_express.js

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+// 6-http_express.js
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(1245, () => {
+  console.log('Server listening on port 1245');
+});
+
+module.exports = app;

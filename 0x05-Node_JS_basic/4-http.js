a3368c24381c0489259ee7820e7cdfadbc0963d3
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2024-08-24 11:35:35 +0100

    Create 4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,9 @@
+// 4-http.js
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+
+module.exports = app;
